5897ac54c1a379a72b9a6d404d338d6f817c8579
chicorycoffee <yk.nonogaki@gmail.com>
2023-07-01 21:10:23 +0900

    リファクタリング

--- src/main/java/in/techcamp/firstapp/PostController.java
+++ src/main/java/in/techcamp/firstapp/PostController.java
@@ -1,27 +1,25 @@
 package in.techcamp.firstapp;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.List;
-
 @Controller
+@RequiredArgsConstructor
 public class PostController {
+    private final PostRepository postRepository;
+
     @GetMapping("/hello")
-    public String showHello(Model model) {
+    public String showHello(Model model){
         var sampleText = "サンプルテキスト";
         model.addAttribute("sampleText", sampleText);
         return "hello";
     }
 
     @GetMapping
-    public String showList(Model model) {
-        var postList = List.of(
-                new PostEntity(1, "投稿１"),
-                new PostEntity(2, "投稿２"),
-                new PostEntity(3, "投稿３")
-        );
+    public String showList(Model model){
+        var postList = postRepository.findAll();
         model.addAttribute("postList", postList);
         return "index";
     }
